ecfb0823c346723c7484587b68e31c27c7c96089
liwei123o0 <877129310@qq.com>
2015-01-08 13:56:22 +0800

    scrapy学习
    scrapy初步学习

---
+++ webbot/webbot/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for webbot project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'webbot'
+
+SPIDER_MODULES = ['webbot.spiders']
+NEWSPIDER_MODULE = 'webbot.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'webbot (+http://www.yourdomain.com)'
