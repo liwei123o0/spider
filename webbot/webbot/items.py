1f97c6904c34e278adf38ad4933bc9ce5afb2400
liwei123o0 <877129310@qq.com>
2015-01-09 14:12:45 +0800

    day-02
    14:12更新

--- webbot/webbot/items.py
+++ webbot/webbot/items.py
@@ -5,31 +5,36 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item,Field
 'items用于存储scrapy抓取到的数据，以及定义数据字段'
-
-class WebbotItem(scrapy.Item):
+class WebbotItem(Item):
     # define the fields for your item here like:
-    name = scrapy.Field()
-    id = scrapy.Field()
+    at = Field()
+    conten = Field()
 
-'创建item并将所存储数据以两种方式打印出来'
-webbot =WebbotItem(name='牛奶吧',id=1)
-print(webbot)
-print(webbot).get('name')
-print(webbot['id'])
 
-'获取字段值'
-print webbot.get('title','标题')
-'获取所有获取到的值'
-print(webbot.keys())
-print(webbot.items())
-
-'''
-扩展items可以通过继承item来扩展item
-添加和修改原始item的字段
-'''
-class newsItem(WebbotItem):
-    title =scrapy.Field()
-    url = scrapy.Field()
-
+class DmozItem(Item):
+    # define the fields for your item here like:
+    title = Field()
+    link = Field()
+    desc = Field()
+#
+# '创建item并将所存储数据以两种方式打印出来'
+# webbot =WebbotItem(name='牛奶吧',id=1)
+# print(webbot)
+# print(webbot).get('name')
+# print(webbot['id'])
+#
+# '获取字段值'
+# print webbot.get('title','标题')
+# '获取所有获取到的值'
+# print(webbot.keys())
+# print(webbot.items())
+#
+# '''
+# 扩展items可以通过继承item来扩展item
+# 添加和修改原始item的字段
+# '''
+# class newsItem(WebbotItem):
+#     title =scrapy.Field()
+#     url = scrapy.Field()
--- webbot/webbot/spiders/tieba_spider.py
+++ webbot/webbot/spiders/tieba_spider.py
@@ -1,19 +1,41 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.selector import HtmlXPathSelector
-from
+import json
+from webbot.items import DmozItem
+from webbot.items import WebbotItem
 class TiebaSpider(scrapy.Spider):
     name='tieba'
     allowed_domains =['baidu.com']
-    start_name=['http://tieba.baidu.com/f?kw=it&ie=utf-8']
+    start_urls=['http://tieba.baidu.com/p/3515571358']
 
     def parse(self, response):
-        hxs =HtmlXPathSelector(response)
-        sites = hxs.path('//li[@class="j_thread_list clearfix"]')
-        items =[]
-        for site in sites:
-            item=WebbotItem()
-            item['id']=site.path('.//div[@class="threadlist_rep_num"]/text()').extract()
-            item['name'] = site.path('.//a[@class="j_th_tit"]')
-            items.append(item)
-        return items
+        loop=response.xpath('//div[contains(@class,"l_post l_post_")]')
+
+        num=0
+        for site in loop:
+            item= WebbotItem()
+            item['at']=site.xpath('//a[@class="p_author_name j_user_card"]/text()').extract()
+            item['conten'] = site.xpath('//div[@class="p_content p_content_nameplate"]').extract()
+            num+=1
+            print ('#####################%s################################'%num)
+            yield item
+
+
+# class DmozSpider(scrapy.Spider):
+#     name = "dmoz"
+#     allowed_domains = ["dmoz.org"]
+#     start_urls = [
+#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+#     ]
+#
+#     def parse(self, response):
+#         num=0
+#         for sel in response.xpath('//ul/li'):
+#             item = DmozItem()
+#             item['title'] = sel.xpath('a/text()').extract()
+#             item['link'] = sel.xpath('a/@href').extract()
+#             item['desc'] = sel.xpath('text()').extract()
+#             num+=1
+#             print('#####################%s################################'%num)
+#             yield item
